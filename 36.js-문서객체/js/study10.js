42d0c5dbff5deaa84dc1c41cb40a7ac62cee109d
sunyoungpp <zkandl5054@gmail.com>
2023-05-25 17:35:29 +0900

    webclass

--- 36.js-문서객체/js/study10.js
+++ 36.js-문서객체/js/study10.js
@@ -1,17 +1,46 @@
 window.onload = function(){
 
-    let slider1 = new Swiper(".station1",{
-        // direction: 'vertical',
-        loop: true,
-        slidesPerView:1.5,
+    let station1 = new Swiper(".station1",{
+        loop:true,
+        direction:"horizontal",
+        // slidesPerView:3, // 슬라이드 개수의 반만큼 최대 적용 가능
+        // spaceBetween:40,
         pagination: {
-            el: ".swiper-pagination",
-          }
-    })
-    let slider2 = new Swiper(".station2",{
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable:true
+        },// html에 pagination 영역이 있어야 실행됨
         navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
+            nextEl: ".csNextBtn",
+            prevEl: ".csPrevBtn",
+          },
+          autoplay:{
+            delay:1000,
+            // pauseOnMouseEnter:true
+          },
+          speed:1000,
+          breakpoints:{
+            320:{
+                slidesPerView:1,
+                spaceBetween:0
+            },
+            768:{
+                slidesPerView:2,
+                spaceBetween:10
+            },
+            1024:{
+                slidesPerView:3,
+                spaceBetween:20
+            }
           }
+    })    
+    
+    document.querySelector(".station1").addEventListener("mouseover",function(){
+        station1.autoplay.stop()
     })
+    document.querySelector(".station1").addEventListener("mouseout",function(){
+        station1.autoplay.start()
+    })
+
+
 }
