41e2cf6f534756a3337e1ad47c36c6c45bc40f18
sunyoungpp <zkandl5054@gmail.com>
2023-06-05 17:35:44 +0900

    webclass

--- pearlabyss-ex/js/common.js
+++ pearlabyss-ex/js/common.js
@@ -3,7 +3,8 @@ window.onload = function(){
     let newsSlider = new Swiper(".news_station",{
        
         pagination: {
-            el: '.swiper-pagination',
+            el: '.news_pagination',
+            clickalbe:true,
           },
           breakpoints:{
             280:{
@@ -16,18 +17,21 @@ window.onload = function(){
           
     });
     
-    let noticeSlider = new Swiper(".notice",{
-        pagination: {
-            el: '.swiper-pagination',
-          },
+    let noticeSlider = new Swiper(".notice_station",{
+      
           breakpoints:{
             280:{
                 slidesPerView: 1,
             },
             768:{
                 slidesPerView: 4,
+                spaceBetween: 20
             }
-          }   
+          }  ,
+          pagination: {
+            el: '.notice_pagination',
+            clickalbe:true,
+          },
         
     })
 }
