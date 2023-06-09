a16939e1ba5641bf449626f4d091dd1ca34bd4d5
sunyoungpp <zkandl5054@gmail.com>
2023-06-09 15:58:09 +0900

    webclass

---
+++ 39.js-effect/js/study4.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    
+    $(".list>li").each(function(){
+        let result = Math.round(Math.random())
+        if(result){
+            $(this).addClass("mltr")
+        }else{
+            $(this).addClass("mrtl")
+        }
+    })
+
+    $(window).scroll(function(){
+        let winst = $(window).scrollTop() // 스크롤바 위에서 얼만큼 내려왔는지 계산
+        let winHeight = $(window).height()*0.65 // 브라우저 화면의 높이를 계산
+
+        $(".mltr,.mrtl").each(function(){
+            if(winst+winHeight>$(this).offset().top){
+                $(this).addClass("on")
+            }else{
+                $(this).removeClass("on")
+            }
+        })
+
+    })
+
+})
