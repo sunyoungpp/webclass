694df15fed15c68c6f03fceede67ccee0ce0edea
sunyoungpp <zkandl5054@gmail.com>
2023-06-19 14:39:04 +0900

    webclass

---
+++ 40.effect/js/study2.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    // let animationTerm = 0.1
+    // let animationDuration = 5
+    // // console.log($(".floatingTxt h1 b").length) // 선택한 태그들은 배열형태로 리턴된다 그래서 배열의 원소의 개수를 .length를 통해 구해볼 수 있다.
+  
+    // for (let i = 0; i <$(".floatingTxt h1 b").length; i++) {
+    //   $(".floatingTxt h1 b:nth-of-type(" + (i + 1) + ")").css("animation", "introAni " + animationDuration + "s ease " + (animationTerm * i) + "s forwards")
+    // }
+   
+
+    
+
+  })
