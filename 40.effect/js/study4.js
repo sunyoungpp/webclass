c1de2357f7dbdad2e2efd1ee5c84c07822a37b9f
sunyoungpp <zkandl5054@gmail.com>
2023-06-21 15:53:42 +0900

    webclass

--- 40.effect/js/study4.js
+++ 40.effect/js/study4.js
@@ -1,10 +1,73 @@
 $(document).ready(function(){
-    let ResultEl = "";
-    for(let i=1; i<=20; i++){
-        ResultEl+=`<li> <img class="small" src="./img/s${i}.jpg" alt="작은이미지1">
-        <img class="big" src="./img/pic${i}.jpg" alt="큰이미지1"></li>`
-    }
-    document.querySelector(".train").innerHTML = ResultEl
+    
+    let result = "";
+    for(let i=1 ; i<=20 ; i++){
+        result+=`<li>
+        <img class="small" src="./img/s${i}.jpg" alt="갤러리${i}이미지">
+        <img class="big" src="./img/pic${i}.jpg" alt="갤러리${i}이미지">
+        <h2 class="title">art work ${i}</h2>
+        <p>Artwork description comes here.<br>2023.06.20</p>
+        <span class="btnClose">close</span>
+        </li>`
+        }
+    // document.querySelector(".train").innerHTML = result
+    $(".train").html(result)
+
+    $(".stationMenu>li").click(function(){
+        let idx = $(this).index() //클릭한 리스트의 순번을 리턴함
+        count = idx*5 // 정해진 순번에 5를 곱한 값을 전역변수 count에 저장함으로써 휠을 내렸을 때 이동되는 기능이 정상적으로 작동되게끔 해주는 코드
+
+        $(".train").css("transform","translateX("+(-1000*idx)+"px)")        
+        
+        // 0 -> 0px
+        // 06 -> -1000px
+        // 11 -> -2000px
+        // 16 -> -3000px
+    })
+
+        let count=0;
+    $(".station").on("wheel DOMMouseScroll",function(event){
+        // console.log(event)
+        let E = event.originalEvent
+        let delta = 0;
+
+        //if 파이어폭스 라면
+        if(E.detail){
+            delta = E.detail * -40
+            //파이어폭스에서 휠 내렸을 때 3이 나왔기 때문에 -120이 나왔던 크롬과 값을 맞추기 위해 곱해준것
+        }else{
+            delta = E.wheelDelta
+
+        }
+        
+        if(delta<0){
+            count++
+            if(count>15){count=15}
+            $(".train").css("transform","translateX("+(-200*count)+"px)") 
+            //마우스 휠을 내렸을 때
+
+        }else{
+            //마우스 휠을 올렸을 때
+            count--
+            if(count<0){count=0}
+            $(".train").css("transform","translateX("+(-200*count)+"px)") 
+        }
+        return false;
+    })
+
+    // 클릭한 리스트(기차 칸)의 활성화 스타일 기능
+    $(".train>li").click(function(){
+        $(".train>li").removeClass("on")
+        $(this).addClass("on")
+        let idx = $(this).index()
+        count = idx // 동기화 시킴
+        $(".train").css("transform","translateX("+(-200*idx)+"px)") 
+    })
+
+    // 닫는버튼 기능
+    $(".btnClose").click(function(){
+        $(".train>li").removeClass("on")
+        return false // 상위태그로 클릭이벤트가 전달되지 않게 (버블링되지않게) 설정
+    })
 
 })
-
