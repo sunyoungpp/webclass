65ce4b1c223a3146cffcedca399b057ca8d64ef5
sunyoungpp <zkandl5054@gmail.com>
2023-06-30 18:57:22 +0900

    web

--- 41.svg/js/study6.js
+++ 41.svg/js/study6.js
@@ -21,10 +21,22 @@ $(document).ready(function () {
         $(".movingArea").css("position", "fixed")
         $(".movingArea").css("top", "0")
         $(".movingArea").css("left", "0")
-        let percent = (distance / (8000 - movingAreaHeight) * 100)
+
+        let percent = (distance / (8000 - movingAreaHeight) * 100) // 0~100 까지의 수
         $("#progress path").css("stroke-dashoffset",pathLength-(pathLength*percent/100))
         $(".per").text(Math.round(percent)+"%")
-  
+
+        let percent2 = (distance / (8000 - movingAreaHeight) * 87.5) // 0~87.5 까지의 수
+        $(".train").css("transform","translateX(-"+percent2+"%)")
+
+        let count = Math.floor(distance/(8000-movingAreaHeight)*8) // 0~7 사이의 정수
+        let count2 = (distance/(8000-movingAreaHeight)*8)-count // 0~1 사이의 소수
+        console.log(count+"번째 리스트의 애니메이션이"+count2+"%만큼 진행되었습니다.")
+        $(".train>li").removeClass("on")
+        $(".train>li").eq(count).addClass("on")
+
+        let count3 = count2*180
+        $(".train li b").css("transform","rotateY("+(180+count3)+"deg)")
       }
   
       if (distance > 8000 - movingAreaHeight) {
